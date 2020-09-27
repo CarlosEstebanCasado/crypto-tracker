e92b3c443d897d4058cc650497f168f2f57396ef
Carlos Esteban <carlosbcn169@gmail.com>
2020-09-27 12:23:45 +0200

    Finished Coin Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
+import Coin from './Coin';
 
 function App() {
   const [coins,setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -13,9 +15,41 @@ function App() {
       .catch(error => console.log(error));
   })
 
+  const handleChange = e => {
+    setSearch(e.target.value);
+  }
+
+  const filteredCoins = coins.filter(coin => {
+    return coin.name.toLowerCase().includes(search.toLocaleLowerCase());
+  })
+
   return (
     <div className="coin-app">
-      <h1>API Crypto</h1>
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input 
+            type="text" 
+            placeholder="Search" 
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return(
+          <Coin 
+            key={coin.id} 
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            price={coin.current_price}
+            marketcap={coin.market_cap}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        )
+      })}
     </div>
   );
 }
