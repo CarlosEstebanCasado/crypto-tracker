1c5167f772ccac9e33c9953d60b07edc7ad7127b
Carlos Esteban <carlosbcn169@gmail.com>
2020-09-27 11:50:41 +0200

    Added API URL conection with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [coins,setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data);
+      })
+      .catch(error => console.log(error));
+  })
+
   return (
-    <div className="App">
+    <div className="coin-app">
       <h1>API Crypto</h1>
     </div>
   );
